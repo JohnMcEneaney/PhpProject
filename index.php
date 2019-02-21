a94347d25dbaf28291df29d3470722bb22cd3e9d
John <John@Johns-PC>
2019-02-21 09:35:53 +0000

    Php project

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "Hello World!"
+        ?>
+        <h1>This is a title</h1>
+        <h2 style="color:red">This is a smaller, red title</h2>
+    </body>
+</html>
